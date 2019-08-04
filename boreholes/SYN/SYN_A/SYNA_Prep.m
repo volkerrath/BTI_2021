a72b1db13ab47334d3c0686a7974224d2fa16f2f
vrath <vrath@cp.dias.ie>
2019-08-04 11:20:26 +0200

    reorganize

---
+++ boreholes/SYN/SYN_A/SYNA_Prep.m
@@ -0,0 +1,324 @@
+function [ierr]=SYN_Prep(name)
+% Site TEMPLIN
+% Prepare Data and Model for inversion
+
+ierr = 0;
+
+load('common.mat')
+
+
+% SET PATHS
+
+addpath([srcpath,'/src']);
+addpath([srcpath,'/tools']);
+addpath([datpath]);
+% 
+% dfmt=1;ffmt='.zip';
+% archive(mfilename,strcat([mfilename '_' datestr(now,dfmt)]),ffmt);
+
+% GENERAL SETTINGS
+
+
+site             = 'SYNA';
+props           = 'syn';
+out             = 0;
+estq            = 1;
+estq_int        = [100 200 300 400 500];
+plotit          = 1;
+
+% INITIAL
+theta           =   1.;                   % time steping weight 1/FI .5/CN
+maxitnl         =   4;                    % number of nl iterations
+tolnl           =   0.00001;
+relaxnl         =  1.;
+freeze          =  1;                     % include freezing/thawing
+
+% PARAMETER FOR PREPROCESSING
+zDatTop         =   10;
+zDatBot         =   2000;
+
+Qb              =  -30*2.3253e+00*1e-3;
+Qbshift         = -0.0000;
+Qb              =   Qb+Qbshift;
+
+GST0            =   0;
+POM             = -4; 
+POR             =   0.00;
+KTop            =   2.3253e+00;   % mean aus TC-m top 100
+KBot            =   2.3253e+00; % mean aus TC-m entire well
+KMean           =   2.3253e+00;
+ErrDeflt        =   0.1;
+L=3;
+CovType = 'g';
+
+
+smooth_props='m';
+avgmeth ='h';
+smooth_data='s';
+nspline=101;
+wspline=0.5;
+
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+% VARIABLES OUTSIDE SYN_PREP OVERWRITE DEFAULTS ABOVE!
+F=strcat([name,'_Prep_in.mat']);
+if exist(F)
+    disp([mfilename ' defaults overwritten!'])
+    load(F); mstruct(prep_in);
+end
+%!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+addpath([srcpath,filesep,strcat(['src/props/',props])]);
+
+
+if plotit
+    set_graphpars
+end
+
+disp(['   ']);
+disp(strcat([ ' ...Preprocessing site ', site]));
+
+step = 0;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% STEP: SPATIAL AND TEMPORAL MESH
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+step=step+1;
+disp(strcat([ ' ...>>> Step ',num2str(step),': set grids']));
+% SPATIAL AND TEMPORAL MESH
+disp([' ...set grids'])
+
+meshfileZ=[name,'_DepthGrid.mat'];
+load (meshfileZ);
+meshfileT=[name,'_TimeGrid.mat'];
+load (meshfileT);
+
+% disp([mfilename '    spatial mesh: ',num2str(nz),' temporal mesh:',num2str(nt)]);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% STEP: READ & PREPROCESS DATA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+step=step+1;
+disp(strcat([ ' ...>>> Step ',num2str(step),': read obs']));
+
+O1    =   importdata([datpath,'/DataBallingA.csv']);
+
+% Version 2012b
+zT = O1(:,1); 
+T = O1(:,4); zT = zT(isfinite(T)); T = T(isfinite(T));
+
+N = length(T(:,1));
+
+Err = ErrDeflt;
+switch lower(CovType)
+    case{'e' 'markov' 'exponential'}
+        Cov=CovarExpnl(Err*ones(N,1),L);
+    case{'g' 'gauss'}
+         Cov=CovarGauss(Err*ones(N,1),L);
+%     case{'m' 'matern'}
+%         Cov=CovarMatern(Err*ones(N,1),L);
+    otherwise
+        error([lower(CovFun), ' not implemented. STOP']);
+end
+
+
+
+
+Cov=CovarGauss(Err*ones(N,1),L);
+C    = chol(Cov);
+err_nor =  0.3*randn(N,1);
+err_cor =  err_nor'*C;
+
+T = T+err_cor';
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+step=step+1;
+disp(strcat([ ' ...>>> Step ',num2str(step),...
+    ': average & interpolate to FD mesh']));
+
+
+% CELL CENTERS
+zm=0.5*(z(1:nz-1)+z(2:nz));nc=length(zm);
+
+% TEMPERATURE
+breaks=linspace(min(zT),max(zT),nspline);
+ss=splinefit(zT,T,breaks,'r',wspline);dd=ppdiff(ss);
+Tx=ppval(ss,z);Tc=ppval(ss,zm);dTc=ppval(dd,zm);
+resT=ppval(ss,zT)-T;no=length(resT);errT=norm(resT)/sqrt(no);
+Ts=NaN(size(z));
+Ts(z>=min(zT) & z <=max(zT))=Tx(z>=min(zT) & z <=max(zT));
+
+
+% CUT LOG
+Ts(z< zDatTop)=NaN;
+Ts(z> zDatBot)=NaN;
+id=find(isfinite(Ts));
+Tobs=Ts(id);zobs=z(id); nd=length(id);Tobs=Tobs';
+
+
+% BULK THERMAL CONDUCTIVITY, RHOB, RHOC, POR
+
+
+K = ones(size(dz))*2.3253e+00;
+POR = ones(size(dz))*0.0001; 
+RHOC = ones(size(dz))*2500;
+RHOB = ones(size(dz))*1000;
+RHP = ones(size(dz))*0.0001e-6;
+
+if plotit
+    % PLOT TEMPERATURES
+    figure
+    plot(T,zT,'.b','LineWidth',1); hold on
+    plot(Tobs,zobs,'-r','LineWidth',1); hold on
+%     xlim([0 50]);
+%     ylim([0 2750]);
+    set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg)
+    xlabel('T (C)','FontSize',fontsz,'FontWeight',fontwg)
+    ylabel('depth (m)','FontSize',fontsz,'FontWeight',fontwg)
+    title(strcat([site, ' temperatures']),'FontSize',fontsz,'FontWeight',fontwg);
+    S1=strcat(['orig']);
+    S2=strcat(['mod']);
+    legend(S1,S2,'location', 'southwest')
+    grid on
+    file=strcat([name '_Temp']);
+    saveas(gcf,file,plotfmt)
+    
+    %
+    %
+    % PLOT CONDUCTIVITIY
+    figure
+    zK = zm;
+    K = K(:);
+    plot(K,zK,'.b','LineWidth',1); hold on
+    plot(K,zm,'-r','LineWidth',2); hold on
+    
+    %ylim([0 2750]);
+    %xlim([0 10]);
+    set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg)
+    xlabel('\lambda (W m^{-1}K^{-1})','FontSize',fontsz,'FontWeight',fontwg)
+    ylabel('depth (m)','FontSize',fontsz,'FontWeight',fontwg)
+    title(strcat([site, ' thermal conductivities']),'FontSize',fontsz,'FontWeight',fontwg);
+    S1=strcat(['orig']);
+    S2=strcat(['mod']);
+    legend(S1,S2,'location', 'southeast')
+    grid on
+    file=strcat([ name '_Lamb']);
+    saveas(gcf,file,plotfmt)
+end
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % STEP III: ESTIMATE Qb FOR CHOSEN DEPTHINTERVAL
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+
+
+if estq
+    step=step+1;
+    disp(strcat([ ' ...>>> Step ',num2str(step),': Estimate Qb - only info']));
+    zTx=zobs';
+    Tx=Tobs;
+    TG=diff(Tx)./diff(zTx);
+    zG= 0.5*(zTx(1:length(zTx)-1)+zTx(2:length(zTx)));
+    Kx=K(id);Kx=Kx(1:length(Kx)-1);
+    whos Kx  TG
+    Qobs = Kx.*TG;
+    
+    
+    for Q_EstDepth=zDatBot-estq_int
+        DepthInterval=zG>Q_EstDepth;
+        Q = Qobs(DepthInterval);
+        Q = Q(isfinite(Q));
+        Qbm = median(Q);Qbt=mad(Q);
+        disp(strcat(['Depth = ', ...
+            num2str(Q_EstDepth),' - ',...
+            num2str(max(zG)) ' m :', ...
+            ' Qmed = ',num2str(Qbm*1000), ...
+            ' Qmad = ',num2str(Qbt*1000), ...
+            ' mW/m**2' ]));
+    end
+    
+    if plotit
+        % PLOT GRADIENTS
+        figure
+        plot(Qobs*1000,zG,'-r','LineWidth',2); hold on
+        %ylim([0 2750]);
+        set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg)
+        xlabel('HFD T (mW/m^2)','FontSize',fontsz,'FontWeight',fontwg)
+        ylabel('depth (m)','FontSize',fontsz,'FontWeight',fontwg)
+        title(strcat([site,' Q_z']),'FontSize',fontsz,'FontWeight',fontwg);
+        % S1=strcat(['original obs']);
+        %legend(S1,'location', 'southwest')
+        grid on
+        file=strcat([ name '_Qz']);
+        saveas(gcf,file,plotfmt)
+        
+        figure
+        plot(TG*1000,zG,'-r','LineWidth',2); hold on
+%         ylim([0 2750]);
+        set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg)
+        xlabel('\nabla T (mW/m^2)','FontSize',fontsz,'FontWeight',fontwg)
+        ylabel('depth (m)','FontSize',fontsz,'FontWeight',fontwg)
+        title(strcat([site,' Qb']),'FontSize',fontsz,'FontWeight',fontwg);
+        % S1=strcat(['original obs']);
+        %legend(S1,'location', 'southwest')
+        grid on
+        file=strcat([ name '_GradT']);
+        saveas(gcf,file,plotfmt)
+        
+    end
+end
+
+
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% STEP IV: DEFINE STRUCTURES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+step=step+1;
+disp(strcat([ ' ...>>> Step ',num2str(step),': setup structure']));
+%
+
+
+
+% STRUCTURE MODEL
+nz=length(z);
+ip=[1:nz-1];nip=length(ip);
+nones=ones(nip,1)';
+ip=ip;
+
+dz=diff(z);
+gts=GST0;
+qb=Qb;
+
+k=K;
+kA=0.00*nones';
+kB=0.00*nones';
+
+
+r=RHOB;
+c=RHOC;
+rc=r.*c;
+
+h = RHP;
+p = POR;
+
+% STRUCTURE DATA
+nd=length(id);
+errT=ErrDeflt;
+Terr=errT*ones(size(id))';
+Tcov=spdiags(Terr.^2,0,nd,nd);
+
+
+sitepar=mstruct(k,kA,kB,h,p,r,c,rc,z,ip,t,it,qb,gts,Tobs,id,zobs,Tcov,Terr,props,name);
+
+
+F=strcat([name '_SitePar.mat']);
+save(F,'sitepar');
+disp([' >>>>> site parameter saved to:' F]);
+disp([' ']);
+
+
+end
---
+++ boreholes/SYN/SYN_A/SYNA_TikhPlot.m
@@ -0,0 +1,252 @@
+% PLOT OF INVERSIONS
+clear all
+close all
+clc
+
+load common
+
+debug=0;
+
+% SET PATHS
+addpath([srcpath,'/tools/'])
+addpath([srcpath,'/src/'])
+
+dfmt=1;ffmt='.zip';
+archive(mfilename,strcat([mfilename '_' datestr(now,dfmt)]),ffmt);
+SITE='SYN';
+
+% LOCAL OPTIONS
+yeartosec=31557600;sectoyear=1/yeartosec;
+
+%GRAPHICS
+plotfmt1='png';
+plotfmt2='epsc2';
+outsteps=false;
+graph=false;
+fontwg='normal';
+fontsz=16;
+cols={'-b','-r','-g','-m','-k' '--b','--r','--g','--m','--k' ':b',':r',':g',':m',':k'};
+
+plot_resd=1;
+plot_gsth=1;
+plot_grad=1;
+plot_tlog=1;
+include_proxies=0;
+smooth_props='m';
+avgmeth ='h';
+smoothpl=1;
+
+smooth_data='m';
+nsplineD=41;wsplineD=0.5;
+smoothdl=1;
+
+smooth_grads = 'm';
+smoothgl=1;
+gmode={'box','mir'};
+
+% 
+% if include_proxies
+%     load([ datpath 'Luterbacher2004_FMK']);
+%     tLut=abs(YL-2002)+2;
+%     TLut=0.71*T30L+2.9; %ILmo
+%     TLut=0.59*T30L+2.76; %Jan
+%     lLut=strcat(['Luterbacher 2004']);
+% end
+
+Tlimits=[-15 20];
+
+zlimits=[0 1800];
+if plot_tlog,
+    tscal=1.;
+    %tlimits=[10 150000];
+    tlimits=[10 150000];
+    
+else
+    tscal=1e-3;
+    tlimits=[0 16];
+end
+
+disp(['   '])
+run='Synthetic - Balling mod A';
+%run = strrep()
+TMP=dir('SYNA_*_results.mat');
+Files={TMP.name};
+%Files={...
+% };
+   
+
+
+Nfiles=length(Files);
+whichfiles=[1:Nfiles];
+nfiles=length(whichfiles);
+
+INFO=fopen('INFO.dat','w');
+imodel=0;
+for ifiles=1:nfiles
+    imodel=imodel+1;
+    filename=Files{ifiles};
+    %disp(['   ']);disp([filename,' rms =',num2str]);
+    load(filename)
+    %reg1par
+    allt{imodel}=t;allit{imodel}=it;
+    [nit,nst]=size(m_iter);
+    allname{imodel}=name;
+    mm=m_iter(nit,:);mm=mm(it);
+    allres{imodel}=r_iter(nit,:)';
+    allgsth{imodel}=mm(:)';
+    allrms{imodel}=rms_iter(nit);
+    allq0{imodel}=qb;
+    allt0{imodel}=gts;
+    allmeth{imodel,1}=0;
+    allmeth{imodel,2}=0;
+    allmeth{imodel,3}=regpar0;
+    allcov{imodel}=Cmm;
+
+    smooth_grads='m';smoothgl=21;gmode={'box','mir'};M=1;
+    zd=z(id);dz=diff(z(id));ii=id(1:length(id)-1)';
+    zm=0.5*(zd(1:length(zd)-1)+zd(2:length(zd)));
+    TC=Tcalc(id,nt);dT=diff(TC);dTC=dT(:)./dz(:);QC = Tcon(ii).*dTC(:);
+    TO=Tobs;dT=diff(TO);dTO=dT(:)./dz(:);Q0 = Tcon(ii).*dTO(:);
+    switch lower(smooth_grads)
+        case {'s','spline'}
+            spoints=linspace(min(zG),max(zG),nsplineG);
+            pp=Q0(:);ss=splinefit(zG,pp,spoints,'r',wsplineG);
+            Qobs=ppval(ss,zG);
+        case {'f','filter','m','mavg'}
+            pp=Q0(:);npp=length(pp);LPROP=smoothgl;
+             ss= wfilt(pp,LPROP,M,gmode);
+            Qobs=ss;
+        otherwise
+            disp([' no smoothing!'])
+    end
+    
+    
+    allTcal{imodel}=TC(:)';
+    allTobs{imodel}=TO(:)';
+    allQcal{imodel}=QC(:)';
+    allQobs{imodel}=Qobs(:)';
+    allTCon{imodel}=Tcon(:)';
+    allzm{imodel}=zm(:)';
+    allzd{imodel}=zd(:)';
+    disp(['   ']);disp([filename,',  rms =',num2str(allrms{imodel})]);
+
+end
+
+%
+% PLOT RESULTS
+disp(['   ']);disp([' ...plot results']);
+%
+% FIG GSTH
+
+[n1,n2]=size(allgsth);
+N=length(allres);
+%
+
+icurv=0;
+for ii=1:N
+    icurv=icurv+1;
+%     %regpval=allmeth{ii,3};sregpval=num2str(regpval(1));
+    rmsval = allrms{ii};
+    q0val  = allq0{ii};
+    legstr{icurv}= ...
+         strcat([num2str(abs(q0val*1000)),'/',num2str(rmsval,'%5.3f')]);
+
+%     %     legstr{icurv}= ...
+%     %         strcat([sregpval,'/',num2str(allrms(ii),'%5.3f'),'/',allmeth{ii,2}]);
+%     
+%     %     legstr{icurv}= ...
+%     %         strcat([run ': ',num2str(allq0(ii)*1000),'/',num2str(allrms(icurv),'%3.2f')]);
+%     legstr{icurv}= ...
+%         strcat([run ': ',QN{ii},'/',num2str(allrms{icurv},'%3.2f')]);
+%     
+end
+% GSTH
+if plot_gsth
+    
+    
+    figure;
+    
+    if plot_tlog
+        for ii=1:N
+               ty=allt{ii}/year2sec+13.5;
+            h(ii)=plot(-ty*tscal,allgsth{ii},cols{ii},'LineWidth',2);hold on
+        end
+%         if include_proxies
+%             legstr=[legstr lLut];
+%             plot(tLut,TLut,'+b','LineWidth',1);
+%             %legstr=[legstr lLuto];
+%             %plot(tLut,TLut_orig,'.m','LineWidth',1);
+%         end
+        xlim(tlimits);
+        set(gca,'xscale','log','xdir','rev',...
+            'xtick',[10 100 1000 10000 100000],...            
+            'FontSize',fontsz,'FontWeight',fontwg);
+        locl='southeast';
+    else
+        for ii=1:N
+            h(ii)=plot(-ty*tscal,allgsth{ii},cols{ii},'LineWidth',2);hold on
+        end
+%         if include_proxies
+%             legstr=[legstr lLut];
+%             plot(tLut,TLut,'+b','LineWidth',1);
+%             %plot(tHist,THist,'*m','LineWidth',1);
+%         end
+
+        xlim(tlimits);
+        set(gca,'xdir','rev',...
+            'FontSize',fontsz,'FontWeight',fontwg);
+        %            'xtick',[0:2:16],...
+        locl='southeast';
+    end
+    title(strcat([SITE,': ',run]),'FontSize',fontsz,'FontWeight',fontwg);
+    legend(legstr,'location','best');
+    grid on;
+    xlabel('Time BP (a)');ylabel('T (C)');
+    filename=strcat(SITE,'_GSTH_',run);
+    saveas(gcf,filename,plotfmt1)
+        saveas(gcf,filename,plotfmt2)
+end
+
+
+%RESIDUALS
+if plot_resd
+    figure;
+    for ii=1:N    
+        plot(allres{ii},allzd{ii},cols{ii},'LineWidth',2);hold on
+    end
+    grid on;
+    set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg);
+    ylabel('depth (m)');xlabel('\Delta T (K)');
+    ylim(zlimits);
+%     xlim([-0.225 0.075]);
+    %xlim([-0.02 0.02]);
+    title(strcat([SITE,': ',run]),'FontSize',fontsz,'FontWeight',fontwg);
+    legend(legstr,'location','northwest');
+    filename=strcat(SITE,'_RESID_',run);
+    saveas(gcf,filename,plotfmt1)
+        saveas(gcf,filename,plotfmt2)
+end
+
+if plot_grad,
+    % GRADIENTS
+    figure
+    plot(allQobs{1}*1000,allzm{1},'-k','LineWidth',3); hold on
+    for ii=1:N
+        plot(allQcal{ii}*1000,allzm{ii},cols{ii},'LineWidth',2); hold on
+    end
+     ylim(zlimits);
+%     xlim([20 60]);
+    set(gca,'ydir','rev','FontSize',fontsz,'FontWeight',fontwg)
+    xlabel('\lambda \nabla T (K/km)','FontSize',fontsz,'FontWeight',fontwg)
+    ylabel('depth (m)','FontSize',fontsz,'FontWeight',fontwg)
+    title(strcat([SITE,': ',run]),'FontSize',fontsz,'FontWeight',fontwg);
+    obsstr = {'observed'};
+    str = [obsstr legstr];
+    legend(str,'location','southwest')
+    grid on
+    filename=strcat([SITE '_Q0',]);
+    %export_fig filename -transparent -png   %-pdf -eps
+    saveas(gcf,filename,plotfmt1)
+        saveas(gcf,filename,plotfmt2)
+end
+
