fb0c9ddce80c7e5915b75667bfdfc444f9f2c102
vrath <vrath@cp.dias.ie>
2019-08-28 11:40:33 +0200

    correction in set_stepgst, pom now parameter

--- src/set_stpgst.m
+++ src/set_stpgst.m
@@ -1,23 +1,27 @@
-function [S,T]=set_stpgst(t,stepamp,steptime,L,debug)
+function [S,T]=set_stpgst(t,stepamp,steptime,L,pom,debug)
 % [S,T]=SET_GST(TIME,STEPAMP,STEPTIME,L,DEBUG) initializes gsth from
 % step function for paleoclimate with amplitude stepamp at time steptime, 
 % given an input vector of temporal nodes at times t. L is the 
 % length of a smoothing operator applied to the temperature time series.
 % if debug > 0, a control plot is produced
 % Last change: vr July 20, 2019
-if nargin < 5, debug=0; end
-if nargin <4, L=0; end
+if nargin < 6, debug=0; end
+if nargin < 5, pom=NaN; end
+if nargin < 4, L=0; end
 
-ns=length(steptime);
-nt=length(t);
+ns=length(steptime); nt=length(t);
+if ~isfinite(pom)
+    pom=stepamp(ns)-4;
+end
+
 maske=find(t<steptime(1));
 % maske
-T(maske)=stepamp(1);
+T(maske)=pom;
 for i=2:ns 
 %     i
     maske=find(t<steptime(i) & t>=steptime(i-1));
 %     maske
-    T(maske)=stepamp(i);
+    T(maske)=stepamp(i-1);
 end
 maske=find(t>steptime(ns));
 T(maske)=stepamp(ns);
